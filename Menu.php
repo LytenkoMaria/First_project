bc17de907f9101e31c02efb7a5535aeb16b95092
LytenkoMaria <lytenko.sbase@gmail.com>
2021-02-17 13:29:10 +0200

    Verificated and + error messege bar

--- Menu.php
+++ Menu.php
@@ -1,7 +1,7 @@
  <div class="menu">
 	<ul class="list-navigation">
 		<li class="nav-item"><a href="./HomePage.php" class="nav-link">Profile</a></li>
-		<li class="nav-item"><a href="./Comments.php" class="nav-link">Coment</a></li>
+		<li class="nav-item"><a href="./Comments.php" class="nav-link">Comments</a></li>
 		<li class="nav-item"><a href="./ExitForm.php" class="nav-link">Exit</a></li>
 	</ul>
  </div>
